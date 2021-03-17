2acf3f71370fa444d9517bead000bd44cb4ddd2f
Indra-artwork <adeindra@students.amikom.ac.id>
2021-03-17 14:13:17 +0700

    Program Taxi Added

---
+++ Taxi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            TaxiClass taxi = new TaxiClass();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger= 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
